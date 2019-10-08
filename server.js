4232052505cd70f945990426410c6cd23e574834
Ricardo Perdomo <hndgd1980@gmail.com>
2019-10-08 18:34:00 -0500

    Add mongo options

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/trumpy";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
 
 // Routes
 
