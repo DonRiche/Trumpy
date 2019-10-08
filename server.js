54afbb489c8f18ce8daf4ddf9f1f332106f0d37d
Ricardo Perdomo <hndgd1980@gmail.com>
2019-10-08 18:27:30 -0500

    Update mongo uri for prod

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/trumpy", { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/trumpy";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
